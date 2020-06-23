de71f259dceab1ad476e8b91c6cacceba4c45608
Álvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-06-23 19:53:41 -0300

    Inimigo Gotinha

---
+++ scripts/jogo/inimigo.js
@@ -0,0 +1,5 @@
+class Inimigo extends Animacao {
+  constructor(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite){
+    super(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite)
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,12 +1,47 @@
 let imagemCenario;
 let imagemPersonagem;
+let imagemInimigo;
+
 let cenario;
 let somDoJogo;
 let personagem;
+let inimigo;
+
+const matrizInimigo = [
+  [0, 0],
+  [104, 0],
+  [208, 0],
+  [312, 0],
+  [0, 104],
+  [104, 104],
+  [208, 104],
+  [312, 104],
+  [0, 208],
+  [104, 208],
+  [208, 208],
+  [312, 208],
+  [0, 312],
+  [104, 312],
+  [208, 312],
+  [312, 312],
+  [0, 418],
+  [104, 418],
+  [208, 418],
+  [312, 418],
+  [0, 522],
+  [104, 522],
+  [208, 522],
+  [312, 522],
+  [0, 626],
+  [104, 626],
+  [208, 626],
+  [312, 626],
+]
 
 function preload() {
   imagemCenario = loadImage("imagens/cenario/floresta.png");
   imagemPersonagem = loadImage("imagens/personagem/correndo.png");
+  imagemInimigo = loadImage("imagens/inimigos/gotinha.png");
   somDoJogo = loadSound('sons/trilha_jogo.mp3');
 }
 
@@ -14,6 +49,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   cenario = new Cenario(imagemCenario, 3);
   personagem = new Personagem(imagemPersonagem);
+  inimigo = new Inimigo(matrizInimigo, imagemInimigo, width - 52, 52, 52, 104, 104)
   frameRate(40)
   somDoJogo.loop();
 }
@@ -21,6 +57,8 @@ function setup() {
 function draw() {
   cenario.exibe()
   cenario.move();
+
+  inimigo.exibe();
 
   personagem.exibe();
 
